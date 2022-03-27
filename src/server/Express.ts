ec6aa194eaf18da75a49d6ad72b815e09c6e41b7
dsl@dosomelight.com <dsl@dch.hldns.ru>
2022-03-27 16:37:46 +0300

    port change to 5010

--- src/server/Express.ts
+++ src/server/Express.ts
@@ -54,8 +54,8 @@ class Express {
     * Starts the express server
     */
    public init(): void {
-      this.server_for_client.listen(5005, () => {
-         console.log('Server started on port 5005');
+      this.server_for_client.listen(5010, () => {
+         console.log('Server started on port 5010');
       });
    }
 }
