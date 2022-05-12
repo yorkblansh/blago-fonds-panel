7390dc1555a05185e84f21ecf9d889ece1994c9c
Oleg Chepelev <yorkblansh@gmail.com>
2022-05-12 18:30:48 +0300

    ✨ feat(home page/cmpnts/header): update tsx

--- react_frontend/src/PAGES/Home_page/components/header/header.tsx
+++ react_frontend/src/PAGES/Home_page/components/header/header.tsx
@@ -1,20 +1,23 @@
 import './header.style.scss';
 
-export interface IHeader {
-   (props: {
-      AuthBtn?: JSX.Element;
-      RegisterBtn?: JSX.Element;
-      Buttons: JSX.Element[];
-      reg_or_auth?: boolean;
-   }): JSX.Element;
+export interface HeaderProps {
+	AuthBtn?: JSX.Element;
+	RegisterBtn?: JSX.Element;
+	Buttons: JSX.Element[];
+	reg_or_auth?: boolean;
 }
 
-export const Header: IHeader = ({ AuthBtn, RegisterBtn, Buttons, reg_or_auth }) => {
-   return (
-      <div className={`header--wrapper ${reg_or_auth && 'header--wrapper--pos_absolute'}`} id="header--wrapper">
-         {Buttons.map((btn, i) => {
-            return <div id={`header--wrapper--element${i}`} children={btn} />;
-         })}
-      </div>
-   );
+export const Header = ({ AuthBtn, RegisterBtn, Buttons, reg_or_auth }: HeaderProps) => {
+	return (
+		<div className={`header--wrapper ${reg_or_auth && 'header--wrapper--pos_absolute'}`} id="header--wrapper">
+			<div className="header--wrapper--title">
+				<div> Панель мониторинга благотворительных фондов</div>
+			</div>
+			<div className="header--wrapper--btns">
+				{Buttons.map((btn, i) => {
+					return <div id={`header--wrapper--element${i}`} children={btn} />;
+				})}
+			</div>
+		</div>
+	);
 };
