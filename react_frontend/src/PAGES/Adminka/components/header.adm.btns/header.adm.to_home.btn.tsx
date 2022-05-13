876c667635ffbc38bf086d6dd5cd3ffbc61919f4
Oleg Chepelev <yorkblansh@gmail.com>
2022-05-13 13:56:07 +0300

    ✨ feat(Header_ADM_BTN):

--- react_frontend/src/PAGES/Adminka/components/header.adm.btns/header.adm.to_home.btn.tsx
+++ react_frontend/src/PAGES/Adminka/components/header.adm.btns/header.adm.to_home.btn.tsx
@@ -2,10 +2,11 @@ import './header.adm.to_home.btn.style.scss'
 import { API, MAIN_PATHES } from 'api/consts'
 
 export interface IHeader_ADM_BTN {
-	(props: { Label: string; path: keyof typeof API | keyof typeof MAIN_PATHES }): JSX.Element
+	Label: string
+	path: keyof typeof API | keyof typeof MAIN_PATHES
 }
 
-export const Header_ADM_BTN: IHeader_ADM_BTN = ({ Label, path }) => {
+export const Header_ADM_BTN = ({ Label, path }: IHeader_ADM_BTN) => {
 	return (
 		<>
 			<div
