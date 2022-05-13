87fed40396ffcdb0aa57c872d2f024165ff09f40
Oleg Chepelev <yorkblansh@gmail.com>
2022-05-13 14:26:26 +0300

    🦄 refactor(item perform btn): rename Iprops
    I_Item_Config_BTN -> Item_Config_BTNprops

--- react_frontend/src/PAGES/Adminka/components/item.perform.btn/item.perform.btn.tsx
+++ react_frontend/src/PAGES/Adminka/components/item.perform.btn/item.perform.btn.tsx
@@ -1,10 +1,10 @@
 import './item.perform.btn.style.scss'
 import { PERF_TYPE } from 'api/consts'
-export interface I_Item_Config_BTN {
+export interface Item_Config_BTNprops {
 	(props: { type: keyof typeof PERF_TYPE; Label: string; _onClick: () => void }): JSX.Element
 }
 
-export const Item_Perform_BTN: I_Item_Config_BTN = ({ type, Label, _onClick }) => {
+export const Item_Perform_BTN: Item_Config_BTNprops = ({ type, Label, _onClick }) => {
 	return (
 		<div className={`adm-edit-btn btn--${type}`} onClick={() => _onClick()}>
 			<div>{Label}</div>
