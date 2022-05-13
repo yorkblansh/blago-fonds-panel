53802bde22fd1210ebf0a2156a985f91da9249ec
Oleg Chepelev <yorkblansh@gmail.com>
2022-05-13 13:57:29 +0300

    🦄 refactor: rename
    IHeader_ADM_BTN -> Header_ADM_BTNprops

--- react_frontend/src/PAGES/Adminka/components/header.adm.btns/header.adm.to_home.btn.tsx
+++ react_frontend/src/PAGES/Adminka/components/header.adm.btns/header.adm.to_home.btn.tsx
@@ -1,12 +1,12 @@
 import './header.adm.to_home.btn.style.scss'
 import { API, MAIN_PATHES } from 'api/consts'
 
-export interface IHeader_ADM_BTN {
+export interface Header_ADM_BTNprops {
 	Label: string
 	path: keyof typeof API | keyof typeof MAIN_PATHES
 }
 
-export const Header_ADM_BTN = ({ Label, path }: IHeader_ADM_BTN) => {
+export const Header_ADM_BTN = ({ Label, path }: Header_ADM_BTNprops) => {
 	return (
 		<>
 			<div
