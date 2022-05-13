624da29f91ea8da8c79394bda9c5bdbffa5cadb3
Oleg Chepelev <yorkblansh@gmail.com>
2022-05-13 18:14:41 +0300

    ✨ feat(Item_Perform_BTN): update:
    - fix props interface
    - add conditions in render
    - add PERFORM_BTN_ICON_MAP

--- react_frontend/src/PAGES/Adminka/components/item.perform.btn/item.perform.btn.tsx
+++ react_frontend/src/PAGES/Adminka/components/item.perform.btn/item.perform.btn.tsx
@@ -1,15 +1,36 @@
 import './item.perform.btn.style.scss'
 import { PERF_TYPE } from 'api/consts'
+import { FunctionComponent, SVGProps } from 'react'
+import { SvgPerformBtn, SVG_MAP } from './svg'
+
 export interface Item_Config_BTNprops {
 	type: keyof typeof PERF_TYPE
 	Label: string
 	_onClick: () => void
+	Icon?: FunctionComponent<SVGProps<SVGSVGElement> & { title?: string | undefined }>
 }
 
-export const Item_Perform_BTN = ({ type, Label, _onClick }: Item_Config_BTNprops) => {
+const PERFORM_BTN_ICON_MAP: { [x in keyof typeof PERF_TYPE]?: keyof typeof SVG_MAP } = {
+	ADD_2_FAVORITE: 'like',
+	REMOVE_FROM_FAVORITE: 'unlike',
+	REMOVE: 'check_no',
+	CREATE: 'check_yes',
+}
+
+export const Item_Perform_BTN = ({ type, Label, _onClick, Icon }: Item_Config_BTNprops) => {
+	const select_svg = PERFORM_BTN_ICON_MAP[type]
+	const isAdmin = type === 'CREATE' || type === 'MODIFY' || type === 'REMOVE'
 	return (
-		<div className={`adm-edit-btn btn--${type}`} onClick={() => _onClick()}>
-			<div>{Label}</div>
-		</div>
+		<>
+			{isAdmin ? (
+				<div className={`adm-edit-btn btn--${type}`} style={{ borderWidth: 3 }} onClick={() => _onClick()}>
+					<div>{Label}</div>
+				</div>
+			) : (
+				<div className={`adm-edit-btn btn--${type}`} style={{ borderWidth: 0 }} onClick={() => _onClick()}>
+					<SvgPerformBtn height="50" width="50" select_svg={select_svg ? select_svg : 'unlike'} />
+				</div>
+			)}
+		</>
 	)
 }
