9acf6a91692734fca883fe8e01a3cd656ce16385
Oleg Chepelev <yorkblansh@gmail.com>
2022-05-13 14:27:31 +0300

    ✨ feat(Item_Config_BTNprops): refomat for component

--- react_frontend/src/PAGES/Adminka/components/item.perform.btn/item.perform.btn.tsx
+++ react_frontend/src/PAGES/Adminka/components/item.perform.btn/item.perform.btn.tsx
@@ -1,10 +1,12 @@
 import './item.perform.btn.style.scss'
 import { PERF_TYPE } from 'api/consts'
 export interface Item_Config_BTNprops {
-	(props: { type: keyof typeof PERF_TYPE; Label: string; _onClick: () => void }): JSX.Element
+	type: keyof typeof PERF_TYPE
+	Label: string
+	_onClick: () => void
 }
 
-export const Item_Perform_BTN: Item_Config_BTNprops = ({ type, Label, _onClick }) => {
+export const Item_Perform_BTN = ({ type, Label, _onClick }: Item_Config_BTNprops) => {
 	return (
 		<div className={`adm-edit-btn btn--${type}`} onClick={() => _onClick()}>
 			<div>{Label}</div>
