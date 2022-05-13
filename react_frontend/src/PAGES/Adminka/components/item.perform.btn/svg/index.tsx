b2fc3bb1620280898eae57ca9eb47c21367d6345
Oleg Chepelev <yorkblansh@gmail.com>
2022-05-13 18:09:53 +0300

    ✨ feat(svgs): add index file for export svg

---
+++ react_frontend/src/PAGES/Adminka/components/item.perform.btn/svg/index.tsx
@@ -0,0 +1,21 @@
+import check_yes from './check_yes.svg'
+import check_no from './check_no.svg'
+import unlike from './unlike.svg'
+import like from './like.svg'
+
+interface SvgPerformBtnProps {
+	select_svg: keyof typeof SVG_MAP
+	height?: string | number
+	width?: string | number
+}
+
+export const SVG_MAP = {
+	like,
+	unlike,
+	check_yes,
+	check_no,
+}
+
+export const SvgPerformBtn = ({ select_svg, height, width }: SvgPerformBtnProps) => {
+	return <img {...{ height, width }} src={SVG_MAP[select_svg]} alt={select_svg} />
+}
