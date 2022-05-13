bb109f0507efe4428cda41d465c58519daec2af5
Oleg Chepelev <yorkblansh@gmail.com>
2022-05-13 19:46:31 +0300

    ✨ feat(item perform btn): update

--- react_frontend/src/PAGES/Adminka/components/item.perform.btn/item.perform.btn.tsx
+++ react_frontend/src/PAGES/Adminka/components/item.perform.btn/item.perform.btn.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable react/jsx-pascal-case */
 import './item.perform.btn.style.scss'
 import { PERF_TYPE } from 'api/consts'
 import { FunctionComponent, SVGProps } from 'react'
 import { SvgPerformBtn, SVG_MAP } from './svg'
+import { FavoriteCounter_div } from 'PAGES/components/favorite.counter.div/favorite.counter.div'
 
 export interface Item_Config_BTNprops {
+	favorite_counter?: number
 	type: keyof typeof PERF_TYPE
 	Label: string
 	_onClick: () => void
@@ -17,7 +20,7 @@ const PERFORM_BTN_ICON_MAP: { [x in keyof typeof PERF_TYPE]?: keyof typeof SVG_M
 	CREATE: 'check_yes',
 }
 
-export const Item_Perform_BTN = ({ type, Label, _onClick, Icon }: Item_Config_BTNprops) => {
+export const Item_Perform_BTN = ({ type, Label, _onClick, favorite_counter }: Item_Config_BTNprops) => {
 	const select_svg = PERFORM_BTN_ICON_MAP[type]
 	const isAdmin = type === 'CREATE' || type === 'MODIFY' || type === 'REMOVE'
 	return (
@@ -28,6 +31,7 @@ export const Item_Perform_BTN = ({ type, Label, _onClick, Icon }: Item_Config_BT
 				</div>
 			) : (
 				<div className={`adm-edit-btn btn--${type}`} style={{ borderWidth: 0 }} onClick={() => _onClick()}>
+					{favorite_counter ? <FavoriteCounter_div {...{ favorite_counter }} /> : null}
 					<SvgPerformBtn height="50" width="50" select_svg={select_svg ? select_svg : 'unlike'} />
 				</div>
 			)}
