9774dcf59d6070b063a87af0002b49adde9429c4
Oleg Chepelev <yorkblansh@gmail.com>
2022-05-13 19:45:30 +0300

    ✨ feat(FavoriteCounter_div): update:
    - fix interface props
    - add favorite_counter

--- react_frontend/src/PAGES/components/favorite.counter.div/favorite.counter.div.tsx
+++ react_frontend/src/PAGES/components/favorite.counter.div/favorite.counter.div.tsx
@@ -1,9 +1,13 @@
 import './favorite.counter.style.scss'
 
-interface IFavoriteCounter_div {
-	(props: { favorite_counter: string | number }): JSX.Element
+interface FavoriteCounter_divProps {
+	favorite_counter: string | number
 }
 
-export const FavoriteCounter_div: IFavoriteCounter_div = ({ favorite_counter }) => {
-	return <div className="favorite-counter">{`Запись добавили в избранное - ${favorite_counter} раз`}</div>
+export const FavoriteCounter_div = ({ favorite_counter }: FavoriteCounter_divProps) => {
+	return (
+		<div className="favorite-counter">
+			<div>{favorite_counter}</div>
+		</div>
+	)
 }
