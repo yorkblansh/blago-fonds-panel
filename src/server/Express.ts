5e0a4df7717d5e8acf0aeb22aa10b5b9c686909d
admin@dosomelight.ru <dsl@host.dosomelight.ru>
2022-05-14 03:34:50 +0300

    change port

--- src/server/Express.ts
+++ src/server/Express.ts
@@ -54,8 +54,8 @@ class Express {
 	 * Starts the express server
 	 */
 	public init(): void {
-		this.server_for_client.listen(6020, () => {
-			console.log('Server started on port 6020')
+		this.server_for_client.listen(5020, () => {
+			console.log('Server started on port 5020')
 		})
 	}
 }
